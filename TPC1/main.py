90cc508c012b39963e783051122b74141d07c7cc
0HenriqueFernandes0 <a95323@alunos.uminho.pt>
2023-02-19 20:57:02 +0000

    inicio

--- TPC1/main.py
+++ TPC1/main.py
@@ -7,10 +7,23 @@ def main():
     lines = file.read().split('\n')
     for line in lines:
         tabela.add(line)
-    tabela.print()
-    tabela.exc1()
-    tabela.exc2()
-    tabela.exc3()
+
+    option =0
+    while option!= -1:
+        option=int(input("""
+0 - distribuição da doença por sexo
+1 - distribuição da doença por escalões etários
+2 - distribuição da doença por níveis de colesterol
+3 - exit
+        """))
+        if option == 0:
+            tabela.exc1()
+        elif option == 1:
+            tabela.exc2()
+        elif option == 2:
+            tabela.exc3()
+        else:
+            option = -1
 
 
 if __name__ == '__main__':
