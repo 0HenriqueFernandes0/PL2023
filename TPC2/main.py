3bcd56e7840064e09e502e845c103c54ef8cc92e
0HenriqueFernandes0 <a95323@alunos.uminho.pt>
2023-02-26 18:42:50 +0000

    tpc2

---
+++ TPC2/main.py
@@ -0,0 +1,37 @@
+
+def main():
+    ON = True
+    contador = 0
+    while(1):
+        text = input("Escreva o seu texto\n")
+
+        start_ON_OFF= False
+        of_signal=0
+
+        for charecter in text:
+
+            if charecter == "O" or charecter == "o":
+                start_ON_OFF = True
+            elif start_ON_OFF == True:
+                if charecter == "N" or charecter == "n" and of_signal==0:
+                    ON = True
+                    start_ON_OFF = False
+                elif charecter == "F" or charecter == "f":
+                    if of_signal == 0:
+                        of_signal +=1
+                    elif of_signal == 1:
+                        ON = False
+                        start_ON_OFF = False
+                        of_signal = 0
+                else:
+                    start_ON_OFF = False
+                    of_signal = 0
+
+            if charecter == "=":
+                print(f"Existem {contador} sequencias de digitos")
+            if ON == True and charecter.isdigit():
+                contador += int(charecter)
+
+
+if __name__ == '__main__':
+    main()
