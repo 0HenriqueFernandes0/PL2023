18df526fc61b2e8d8de61af790f35c07cb8286c6
0HenriqueFernandes0 <a95323@alunos.uminho.pt>
2023-03-19 22:38:27 +0000

    inserir feito

--- TPC5/aux1.py
+++ TPC5/aux1.py
@@ -1,5 +1,34 @@
+import re
+
 def ligar(numero,saldo):
-    None
+    m = re.match("^601",numero)
+    n = re.match("^641",numero)
+    if m or n:
+        print("""maq: "Esse número não é permitido neste telefone. Queira discar novo número!" """)
+    else:
+        m = re.match("^00",numero)
+        if m:
+
 
 def inserir(groups,saldo):
-    None
+    texto = """maq: " """
+    for moeda in groups:
+        if moeda:
+            m =re.split("c",moeda)
+            if len(m)>1:
+                m=int(m[0])
+                if m == 1 or m == 5 or m == 10 or m == 20 or m == 50:
+                    saldo[0]+=m
+                else:
+                    texto+=f"{m}c - moeda inválida; "
+            else:
+                m =re.split("e",moeda)
+                if len(m)>1:
+                    m=int(m[0])
+                    if m == 1 or m == 2:  
+                        saldo[1]+=m
+                    else:
+                        texto+=f"{m}c - moeda inválida; "
+    texto+=f"""saldo = {saldo[1]}e{saldo[0]}c" """
+    print(texto)
+
--- TPC5/main.py
+++ TPC5/main.py
@@ -7,41 +7,51 @@ def main():
     #3-ABORTAR -> 0
     #4-PAUSAR -> 0
     #5- fechar pograma(nao é um estado como os outros)
-    Estado = 4
-    saldo = 0
+    Estado = 0
+    saldo = [0,0]
     while Estado < 5 :
         text = input()
         if Estado == 0:
-            m = re.match("MOEDA( \d+,)*( \d)",text)
+            m = re.match("^MOEDA( \d+c,| \d+e,)*( \d+c| \d+e)$",text)
             if m:
-                saldo = inserir(m)
+                m=re.split(",",re.split("A",text)[1])
+                inserir(m,saldo)
                 Estado = 1
             elif re.match("ABORTAR",text):
                 Estado = 3
+                print(f"""maq: "troco={saldo}; Volte sempre!" """)
+                saldo=0
             else:
                 print("Só pode inserir moedas ou entao abortar.")
         elif Estado == 1:
-            m = re.match("T=(\d+)",text)
+            m = re.match("^T=(\d+)$",text)
             if m:
-                saldo-= ligar(m.group(1))
+                print(ligar(m.group(1),saldo))
                 Estado = 2
             elif re.match("ABORTAR",text):
                 Estado = 3
+                print(f"""maq: "troco={saldo}; Volte sempre!" """)
+                saldo=0
             else:
                 print("Só pode ligar ou entao abortar.")
         elif Estado == 2:
             m = re.match("T=(\d+)",text)
             if m:
-                saldo-= ligar(m.group(1))
+                print(ligar(m.group(1),saldo))
             elif re.match("ABORTAR",text):
                 Estado = 3
+                print(f"""maq: "troco={saldo}; Volte sempre!" """)
+                saldo=0
             elif re.match("PAUSAR",text):
                 Estado = 4
+                print(f"""maq: "troco={saldo}; Volte sempre!" """)
+                saldo=0
             else:
                 print("Só pode ligar ou entao pousar/abortar.")
         elif Estado == 3 or Estado == 4:
             if re.match("LEVANTAR",text):
                 Estado = 0
+                print("""maq: "Introduza moedas." """)
             else:
                 print("Telefone ainda nao foi levantado.")
 
